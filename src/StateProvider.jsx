812279c1950b53de24cc55157135479af992884a
Aditya Prasad <adityaprasad20799@gmail.com>
2025-03-29 19:33:14 +0530

    provider check

--- src/StateProvider.jsx
+++ src/StateProvider.jsx
@@ -1,11 +1,21 @@
+
 import React, { createContext, useContext, useReducer } from "react";
 
-export const StateContext = createContext();
+export const StateContext = createContext(null); 
 
-export const StateProvider = ({ reducer, initialState, children }) => (
-  <StateContext.Provider value={useReducer(reducer, initialState)}>
-    {children}
-  </StateContext.Provider>
-);
+export const StateProvider = ({ reducer, initialState, children }) => {
+  const stateValue = useReducer(reducer, initialState); 
+  return (
+    <StateContext.Provider value={stateValue}>
+      {children}
+    </StateContext.Provider>
+  );
+};
 
-export const useStateValue = () => useContext(StateContext);
+export const useStateValue = () => {
+  const context = useContext(StateContext);
+  if (!context) {
+    throw new Error("useStateValue must be used within a StateProvider");
+  }
+  return context;
+};
